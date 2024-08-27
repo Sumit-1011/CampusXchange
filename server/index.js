54085b1f0406c8ae3ee7899eb7774e87f7370169
Sumit Kumar Mishra <sumitkm101102@gmail.com>
2024-08-27 23:09:34 +0530

    cors updated

--- server/index.js
+++ server/index.js
@@ -9,7 +9,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://campusxchange-client.onrender.com",
+  })
+);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
