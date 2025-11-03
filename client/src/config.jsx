a378bda1885db916fa2ca3defcc62a44e565afb0
Sumit <sumitkm101102@gmail.com>
2025-11-03 22:47:56 +0530

    update config frontend

--- client/src/config.jsx
+++ client/src/config.jsx
@@ -1,11 +1,13 @@
 // config.js
+
 const config = {
   apiBaseUrl:
-    import.meta.env.VITE_API_URL ??
+    import.meta.env.VITE_PRIMARY_API_URL ||
     (import.meta.env.MODE === "development"
-      ? "http://localhost:8080"
-      : "https://campusxchange-server.onrender.com"),
+      ? import.meta.env.VITE_LOCAL_API_URL
+      : import.meta.env.VITE_FALLBACK_API_URL),
+
+  fallbackApiBaseUrl: import.meta.env.VITE_FALLBACK_API_URL,
 };
 
 export default config;
-
